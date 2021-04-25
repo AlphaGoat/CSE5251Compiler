28cea667ff7a87bd77e218b484eefc2728324cfc
AlphaGoat <pjtpeterjohnthomas@gmail.com>
2021-04-25 12:26:53 -0500

    Removed print lines in register allocation

--- src/Sparc/RegisterAllocation.java
+++ src/Sparc/RegisterAllocation.java
@@ -36,7 +36,6 @@ public class RegisterAllocation {
 			// registers that do not already have a mapping in the
 			// frame table to available registers
 			assem.Instruction instr = ilist.get(i);
-			System.out.println(instr.format());
 			if (instr instanceof assem.OperationInstruction ||
 					instr instanceof assem.MoveInstruction) {
 				// Go through sources and allocate registers if
@@ -141,7 +140,6 @@ public class RegisterAllocation {
 								}
 								else if (global_count == 2) {
 									newDests.add(currFrame.globalThreeRegister.temp);
-									System.out.println("global three as dest");
 									global_count++;
 								}
 								else {
